babefbb09d858bbb638bb7945212dfceaa293bfb
Foca <a.vinicius0042@gmail.com>
2022-10-29 22:48:30 -0300

    solved

---
+++ dart/lib/tribonacci_sequence.dart
@@ -0,0 +1,11 @@
+//https://www.codewars.com/kata/556deca17c58da83c00002db/solutions/dart
+
+List<num> tribonacci(List<num> signature, int n) {
+  if (n > 2) {
+    for (var i = 0; i < n - 3; i++) {
+      signature.add(signature[i] + signature[i + 1] + signature[i + 2]);
+    }
+    return signature;
+  }
+  return signature.sublist(0, n);
+}
