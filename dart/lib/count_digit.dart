c1f00d89cebc21c390e5e46f6b12f6e114353371
Foca <a.vinicius0042@gmail.com>
2022-11-02 21:20:52 -0300

    solved

---
+++ dart/lib/count_digit.dart
@@ -0,0 +1,26 @@
+int nbDig(int n, int d) {
+  int count = 0;
+  List<int> l = [
+    for (int i = 0; i <= n; i++)
+      if ((i * i).toString().contains(d.toString())) (i * i)
+  ];
+
+  var x = l.map((e) => e.toString().split('')).toList();
+
+  x.forEach((element) {
+    element.forEach((element_) {
+      if (element_ == d.toString()) {
+        count += 1;
+      }
+    });
+  });
+
+  return count;
+
+  //Remember using allMatches for this
+  //d.toString.allMatches(all numbers)
+}
+
+void main(List<String> args) {
+  print(nbDig(5750, 0));
+}
